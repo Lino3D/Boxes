8207f8e33cd1827c225ece169d83276e60501904
Michał <michcio91@gmail.com>
2016-04-09 17:33:48 +0200

    123

--- Algorytmy Zaawansowane/Classes/Box.cs
+++ Algorytmy Zaawansowane/Classes/Box.cs
@@ -34,5 +34,23 @@ namespace Algorytmy_Zaawansowane.Classes
             }
         }
 
+        public override bool Equals(System.Object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Box p = obj as Box;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (boxHeight == p.boxHeight) && (boxWidth == p.boxWidth);
+        }
     }
 }
