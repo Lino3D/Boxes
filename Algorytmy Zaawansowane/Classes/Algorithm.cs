c8182dcb5d4e318203ebc6e14f0edb4f771c6ac8
Michał <michcio91@gmail.com>
2016-04-09 17:34:24 +0200

    123

---
+++ Algorytmy Zaawansowane/Classes/Algorithm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy_Zaawansowane.Classes
+{
+    public static class Algorithm
+    {
+        public int[,] NajdluzszyWspolnyPodciag( List<Box> A, List<Box> B)
+        {
+            if( A.Count() != B.Count() )
+                throw( new Exception("Ilosc pudelek w obu listach sie nie zgadza"));
+            int n = A.Count();
+            int[,] C = new int[n,n];
+            
+            for( int i = 0; i < n; i++)
+                C[i,0] = 0;
+            for( int j = 0; j < n; j++)
+                C[0,j] = 0;
+
+            for( int i = 1; i < n; i++)
+            {
+                for( int j = 1; j < n; j++)
+                {
+                    if( A.ElementAt(i).Equals(B.ElementAt(j)))
+                        C[i,j] = C[i-1,j-1]+1;
+                    else
+                        C[i,j] = Math.Max( C[i-1,j], C[i,j-1]);
+                }
+            }
+
+            return C;
+        }
+
+
+    }
+}
---
+++ Algorytmy Zaawansowane/Classes/BoxList.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy_Zaawansowane.Classes
+{
+    public class BoxList
+    {
+        private List<Box> BoxList = new List<Box>();
+
+        public void DodajPudelka(Box box)
+        {
+            if (box != null)
+                BoxList.Add(box);
+        }
+
+        public void DodajPudelka(List<Box> lstbox)
+        {
+            if (lstbox != null)
+                foreach (var item in lstbox)
+                    BoxList.Add(item);
+        }
+
+        public int GetBoxNumber()
+        {
+            return BoxList.Count;
+        }
+
+        public List<Box> GetBoxList()
+        {
+            return BoxList;
+        }
+
+        public void SortujPudelka(bool SortujPoWysokosci)
+        {
+            if (SortujPoWysokosci)
+                QuicksortHeight(BoxList, 0, BoxList.Count - 1);
+            else
+                QuicksortWidht(BoxList, 0, BoxList.Count - 1);
+        }
+
+
+        private static void QuicksortHeight(List<Box> elements, int left, int right)
+        {
+            int i = left, j = right;
+            Box pivot = elements.ElementAt((left + right) / 2);
+
+            while (i <= j)
+            {
+                while (elements.ElementAt(i).Height < pivot.Height)
+                    i++;
+
+                while (elements.ElementAt(j).Height > pivot.Height)
+                    j--;
+
+                if (i <= j)
+                {
+                    Box tmp = elements.ElementAt(i);
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                QuicksortHeight(elements, left, j);
+            if (i < right)
+                QuicksortHeight(elements, i, right);
+        }
+
+        private static void QuicksortWidht(List<Box> elements, int left, int right)
+        {
+            int i = left, j = right;
+            Box pivot = elements.ElementAt((left + right) / 2);
+
+            while (i <= j)
+            {
+                while (elements.ElementAt(i).Width < pivot.Width)
+                    i++;
+
+                while (elements.ElementAt(j).Width > pivot.Width)
+                    j--;
+
+                if (i <= j)
+                {
+                    Box tmp = elements.ElementAt(i);
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                QuicksortWidht(elements, left, j);
+            if (i < right)
+                QuicksortWidht(elements, i, right);
+        }
+    }
+}
