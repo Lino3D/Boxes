62db60eb4b1c003d9ca0b3f2e7a7557110df8242
Lino3D <kamiskijakub@gmail.com>
2016-04-09 16:46:51 +0200

    Added open file dialogs

--- Algorytmy Zaawansowane/Classes/Box.cs
+++ Algorytmy Zaawansowane/Classes/Box.cs
@@ -34,5 +34,6 @@ namespace Algorytmy_Zaawansowane.Classes
             }
         }
 
+
     }
 }
--- Algorytmy Zaawansowane/MainWindow.xaml.cs
+++ Algorytmy Zaawansowane/MainWindow.xaml.cs
@@ -25,5 +25,33 @@ namespace Algorytmy_Zaawansowane
         {
             InitializeComponent();
         }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void save_as_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+
+                string filename = dlg.FileName;
+            }
+
+
+        }
     }
 }
