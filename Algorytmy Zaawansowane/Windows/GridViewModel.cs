177885eb3a69c4f667e1956028050780eb75635a
Michał <michcio91@gmail.com>
2016-04-20 18:33:24 +0200

    plik brakujacy

---
+++ Algorytmy Zaawansowane/Windows/GridViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy_Zaawansowane.Windows
+{
+    public class GridViewModel : INotifyPropertyChanged
+    {
+        private bool isVisible = false;
+
+        public bool IsVisible
+        {
+            get
+            {
+                return isVisible;
+            }
+
+            set
+            {
+                isVisible = value;
+                NotifyPropertyChanged("IsVisible");
+            }
+        }
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
